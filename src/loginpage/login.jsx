9a929e55967661847de065ae1b0675f4dff64e4d
oleksii danyliuk <nestleguy228@icloud.com>
2023-04-25 22:02:24 +0300

    modified:   src/UI/button/button.css 	modified:   src/index.css 	modified:   src/loginpage/login.jsx 	modified:   src/mainpage/home.jsx 	new file:   src/style/home.css 	modified:   src/style/login.css

--- src/loginpage/login.jsx
+++ src/loginpage/login.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../style/login.css'; 
 
 function Login() {
     return (
-        <div class="main">
+        <div className='bodyLogin'>
+        <div class="mainLogin">
         <input type="checkbox" id="chk" aria-hidden="true"></input>
         <div class="signup">
             <form>
@@ -11,7 +13,7 @@ function Login() {
                 <input type="text" name="txt" placeholder="User name" required=""></input>
                 <input type="email" name="email" placeholder="Email" required=""></input>
                 <input type="password" name="pswd" placeholder="Password" required=""></input>
-                <button>Sign up</button>
+                <Link to="/" className="button" >Sing in</Link>
             </form>
         </div>
         <div class="login">
@@ -19,9 +21,10 @@ function Login() {
                 <label for="chk" aria-hidden="true">Login</label>
                 <input type="email" name="email" placeholder="Email" required=""></input>
                 <input type="password" name="pswd" placeholder="Password" required=""></input>
-                <button>Login</button>
+                <Link to="/" className="button" >Login</Link>
             </form>
         </div>
+    </div>
     </div>
 
     )
--- src/mainpage/home.jsx
+++ src/mainpage/home.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../UI/button/button.css'; 
+import '../style/home.css';
 
 const Home = () => {
   return (
-    <div>
-      <h1>Welcome to the Home page!</h1>
+    <div className="home-container">
+      <div className='tasks'>
+        <div>
       <Link to="/login" className="button" >Go to Login</Link>
+        </div>
+      </div>
+      
+
+      
+
     </div>
   );
 };
